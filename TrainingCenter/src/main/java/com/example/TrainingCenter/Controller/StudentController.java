7c2e796a252aa10c6e0605a564f1a38f11cc785d
mohammeddheshamm <muhamedhesham632@gmail.com>
2024-09-10 23:13:27 +0300

    Security Authorization ADDED

--- TrainingCenter/src/main/java/com/example/TrainingCenter/Controller/StudentController.java
+++ TrainingCenter/src/main/java/com/example/TrainingCenter/Controller/StudentController.java
@@ -3,6 +3,8 @@ package com.example.TrainingCenter.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +21,7 @@ import com.example.TrainingCenter.Entity.Keys.StudentCourseKey;
 import com.example.TrainingCenter.Service.StudentCourseServiceImpl;
 import com.example.TrainingCenter.Service.StudentServiceImpl;
 
+@EnableMethodSecurity
 @RestController
 @RequestMapping(value = "/student")
 public class StudentController {
@@ -29,17 +32,20 @@ public class StudentController {
 	@Autowired
 	StudentCourseServiceImpl stdCrs;
 	
+	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('INSTRUCTOR')")
 	@GetMapping(value = "/getAll", 
 			produces = "application/json")
 	public List<Student> getAll(){
 		return std.getAllElements();
 	}
 	
+	@PreAuthorize("hasAuthority('STUDENT') or hasAuthority('ADMIN') or hasAuthority('INSTRUCTOR')")
 	@GetMapping("/getStudentById/{id}")
 	public Student findStudent(@PathVariable Long id) {
 		return std.getElementById(id);
 	}
 	
+	@PreAuthorize("hasAuthority('STUDENT') or hasAuthority('ADMIN') or hasAuthority('INSTRUCTOR')")
 	@PostMapping(value = "/addStudent",
 			consumes = "application/json",
 			produces = "application/json")
@@ -47,6 +53,8 @@ public class StudentController {
 		return std.addElement(student);
 	}
 	
+	
+	@PreAuthorize("hasAuthority('STUDENT') or hasAuthority('ADMIN')")
 	@PutMapping(value = "/updateStudent",
 			consumes = "application/json",
 			produces = "application/json")
@@ -54,16 +62,21 @@ public class StudentController {
 		return std.updateElement(student);
 	}
 	
+	
+	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('INSTRUCTOR')")
 	@DeleteMapping("/deleteStudent")
 	public String deleteStudent(@RequestBody Student student) {
 		return std.deleteElement(student);
 	}
 	
+	@PreAuthorize("hasAuthority('STUDENT') or hasAuthority('ADMIN') or hasAuthority('INSTRUCTOR')")
 	@GetMapping("/getRegisteredCourses/{id}")
 	public List<Course> getAllCoursesRegisteredByStudentId(@PathVariable Long stdId){
 		return stdCrs.getAllCoursesRegisteredByStudentId(stdId);
 	}
 	
+	
+	@PreAuthorize("hasAuthority('STUDENT') or hasAuthority('ADMIN') or hasAuthority('INSTRUCTOR')")
 	@GetMapping(value = "/getCourseGrade/{stdId}/crsId",
 			produces = "application/json")
 	public CourseGradeDto getCourseWithGrade(@PathVariable Long stdId,Long crsId) {
